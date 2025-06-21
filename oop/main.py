946afeec48d5e57e9250431dce69b1604d2c807b
yoh-ad <yohad2004@gmail.com>
2025-06-21 21:18:35 +0300

    main.py

--- oop/main.py
+++ oop/main.py
@@ -42,3 +42,16 @@ if __name__ == "__main__":
 
 if __name__ == "__main__":
     main()
+
+
+from class_static_methods_demo import Calculator
+
+def main():
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
+
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
+
+if __name__ == "__main__":
+    main()
